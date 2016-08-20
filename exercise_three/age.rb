ff370211057b02be256777c023963e642961a3e4
Thomas Williams <twill14@gmail.com>
2016-08-19 20:08:56 -0500

    Lesson Three Content

---
+++ exercise_three/age.rb
@@ -0,0 +1,13 @@
+puts "How old are you?"
+
+answer = gets.chomp.to_i
+
+answer+=10
+
+  puts "In 10 years you will be #{answer}: " 
+    answer+=10
+  puts "In 20 years, you will be #{answer}: " 
+    answer+=10
+  puts "In 30 years, you will be #{answer}: " 
+    answer+=10
+  puts "In 40 years, you will be #{answer}: " 
---
+++ exercise_three/name.rb
@@ -0,0 +1,29 @@
+#Name Program
+
+puts "Please Enter Your Name:"
+
+name = gets.chomp
+
+puts "Hello " + name + " it's nice to meet you!"
+
+#Display Name 10 Times
+
+puts "Please Enter Your Name:"
+
+name_two = gets.chomp
+
+10.times do |n|
+  puts name_two
+  end
+
+  #Full Name
+
+  puts "Please Enter First Name:"
+
+  first_name = gets.chomp
+
+  puts"Please Enter Last Name:"
+
+  last_name = gets.chomp
+
+  puts"The name you have entered was #{first_name} #{last_name}!"
